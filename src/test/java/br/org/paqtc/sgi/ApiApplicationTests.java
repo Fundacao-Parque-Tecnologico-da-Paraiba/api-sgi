fc5420397f5bf5faa56bc032e4bd5d4d7645f6dc
franciscodantas <francisco.antonio.sousa@ccc.ufcg.edu.br>
2025-08-22 13:47:44 -0300

    first commit

---
+++ src/test/java/br/org/paqtc/sgi/ApiApplicationTests.java
@@ -0,0 +1,11 @@
+package br.org.paqtc.sgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
